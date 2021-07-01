391730c3b6d6bcaf6c15a4cf84febd3bc42e9c4a
ivan alejandro sambrana <=>
2021-07-01 15:48:17 -0300

    https_schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+        if(env('APP_HTTPS'))
+        {
+            \URL::forceScheme('https');
+        }
     }
 }
