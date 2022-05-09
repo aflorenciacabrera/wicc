50b1293310b9d1fdb9b0df295adbfcfe8c04a685
ivan alejandro sambrana <=>
2022-05-09 05:11:29 -0300

    tematica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,6 +86,7 @@
      </div>
      <hr class="hr">
      {{-- ----------******************** --}}
+     
      <div class="row justify-content-center pt-3">
         <div class="col-md-10  text-center ">
             
@@ -130,6 +131,35 @@
                   
         </div>
     </div>
+    
+     <hr class="hr">
+     <div class="row justify-content-center pt-3">
+        <p ><h2><strong class="text-morado">Temáticas de interés de las jornadas</strong></h2></p>
+     </div>
+     <div class="col-md-10 offset-md-1 text-justify" style="font-size: 1.2em">
+      <p>
+        <ul><li>Evaluación de aprendizajes de secuencias didácticas para el aula sobre contenidos
+            de CC</li>
+            <li>Formatos, contenidos y estrategias de formación docente contínua en CC</li>
+            <li>Percepciones sobre la enseñanza de CC por parte de docentes en ejercicio o</li>
+            recientemente formados
+            <li>Narrativa de experiencias áulicas de enseñanza de las CC en nivel inicial, primario o</li>
+            secundario
+            <li>Formatos, contenidos y estrategias de formación inicial docente en CC</li>
+            <li>El Impacto de la enseñanza de CC en la brecha digital</li>
+            <li>Impacto de la enseñanza de CC en la brecha de género</li>
+            <li>Accesibilidad: enseñanza de las CC y programación de forma inclusiva</li>
+            <li>Herramientas y entornos para enseñar CC con distintos dispositivos, con y sin</li>
+            conectividad
+            <li>Estudios comparativos de diseños curriculares y Frameworks</li>
+            <li>Estudio de casos a nivel del aula, la escuela o un sistema educativo</li>
+            <li>Preconceptos de estudiantes con respecto a programación y CC en general</li>
+            <li>Jerarquización de contenidos de las CC en el contexto de la pandemia de Covid 19</li>
+            <li>Aprendizajes y desarrollo cognitivo a partir de la enseñanza de la programación</li>
+            </ul>
+      </p>
+        
+     </div>
      <hr class="hr">
     <div class="row justify-content-center pt-3">
         <div class="col-md-10  text-center  ">
