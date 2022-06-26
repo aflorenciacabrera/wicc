48c0b7926f746dae8b3e8fb461e271b70f6be4d1
ivan sambrana <sambranaivan@gmail.com>
2022-06-25 21:23:47 -0300

    comite

---
+++ resources/views/layouts/comite.blade.php
@@ -0,0 +1,235 @@
+ <div class="row justify-content-center pt-3">
+     <p>
+     <h2><strong class="text-morado">Chairs</strong></h2>
+     </p>
+ </div>
+ <div class="col-md-10 offset-md-1 text-justify" style="font-size: 1.2em">
+     <table class="table table-light">
+
+       <tbody>
+        <tr>
+            <td>
+                <ul>
+                           <li>
+             <b>Emanuel
+                 Irrazábal
+             </b></br>
+            FaCENA - UNNE
+         </li>
+         <li>
+             <b>Gladys
+                 Dapozo
+             </b></br>
+             FaCENA - UNNE
+         </li>
+         <li>
+             <b>Christian
+                 Cossio-Mercado
+             </b></br>
+             Universidad de Buenos Aires. Facultad de Ciencias Exactas y Naturales. Departamento de Computación.
+             
+         </li>
+                </ul>
+            </td>
+        </tr>
+       </tbody>
+
+     </table>
+
+ </div>
+ <hr class="hr">
+ <div class="row justify-content-center pt-3">
+
+     <p>
+     <h2><strong class="text-morado">Comité Académico</strong></h2>
+     </p>
+ </div>
+ <div class="col-md-10 offset-md-1 text-justify" style="font-size: 1.2em">
+     <table class="table table-light">
+         <tbody>
+             <tr>
+                 <td>
+                     <ul>
+
+                         <li><b>Araceli N.
+                                 Acosta
+                             </b></br>FAMAF - Universidad Nacional de Córdoba</li>
+
+
+                         <li><b>Claudia
+                                 Banchoff
+                             </b></br>UNLP</li>
+
+
+                         <li><b>Francisco
+                                 Bavera
+                             </b></br>UNRC</li>
+
+
+                         <li><b>Veronica Andrea
+                                 Bollati
+                             </b></br>CONICET - UNIVERSIDAD TECNOLÓGICA NACIONAL</li>
+
+
+                         <li><b>Claudia
+                                 Casariego
+                             </b></br>UNLa</li>
+                         <li><b>Ana María Company
+                             </b></br>
+                             Universidad Nacional del Nordeste
+                         </li>
+
+                         <li><b>Marcela
+                                 Daniele
+                             </b></br>Universidad Nacional de Río Cuarto</li>
+
+
+                         <li><b>Claudia
+                                 Deco
+                             </b></br>Universidad Nacional de Rosario</li>
+
+
+                         <li><b>Jacqueline
+                                 Fernández
+                             </b></br>Universidad Nacional de San Luis</li>
+
+
+                         <li><b>Emmanuel
+                                 Frati
+                             </b></br>Universidad Nacional de Chilecito</li>
+
+
+                         <li><b>Laura Virginia
+                                 Garbarini
+                             </b></br>Universidad Nacional de Lanús</li>
+
+
+                         <li><b>María Fernanda
+                                 Golobisky
+                             </b></br>CONICET</li>
+
+
+                         <li><b>Marcos
+                                 Gómez
+                             </b></br>Fundación Dr. Manuel Sadosky</li>
+
+
+                         <li><b>Marta
+                                 Lasso
+                             </b></br>Universidad Nacional de la Patagonia Austral</li>
+
+                     </ul>
+                 </td>
+                 <td>
+                     <ul>
+ <li><b>Maria Carmen
+                                 Leonardi
+                             </b></br>UNCPBA</li>
+
+
+                         <li><b>Matías
+                                 Lopez-Rosenfeld
+                             </b></br>UBA-CONICET</li>
+
+
+                         <li><b>Sonia I.
+                                 Mariño
+                             </b></br>FaCENA - UNNE</li>
+
+
+                         <li><b>Pablo E.
+                                 Martínez López
+                             </b></br>UNQ</li>
+
+
+                         <li><b>Flavia
+                                 Millan
+                             </b></br>UNSJ</li>
+
+
+                         <li><b>Natalia
+                                 Monjelat
+                             </b></br>IRICE (CONICET-UNR)</li>
+
+
+                         <li><b>Noelia Soledad
+                                 Pinto
+                             </b></br>UTN FRRe</li>
+
+
+                         <li><b>Fernando
+                                 Puricelli
+                             </b></br>Universidad Nacional de Hurlongham</li>
+
+
+                         <li><b>Nora
+                                 Reyes
+                             </b></br>UNSL</li>
+
+
+                         <li><b>Jorge
+                                 Rodríguez
+                             </b></br>Universidad Nacional del Comahue</li>
+
+
+                         <li><b>Lucila
+                                 Romero
+                             </b></br>UNL</li>
+
+
+                         <li><b>Alfredo Héctor
+                                 Sanzo
+                             </b></br>UTN/UNQ</li>
+
+
+                         <li><b>Pablo
+                                 Turjanski
+                             </b></br>Universidad de Buenos Aires. </br>Facultad de Ciencias Exactas y Naturales.
+                            </br>Departamento de Computación
+                         </li>
+
+                         <li><b>Silvia
+                                 Villagra
+                             </b></br>UNPA-UACO</li>
+                     </ul>
+                 </td>
+             </tr>
+         </tbody>
+     </table>
+
+
+ </div>
+
+ <hr class="hr">
+ <div class="row justify-content-center pt-3">
+     <p>
+     <h2><strong class="text-morado">Comité Organizador FaCENA-UNNE:</strong></h2>
+     </p>
+ </div>
+ <div class="col-md-10 offset-md-1 text-justify" style="font-size: 1.2em">
+<table class="table table-light">
+    <tbody>
+        <tr>
+            <td>
+                  <ul>
+         <li><b>Gladys Dapozo (Coordinadora)</b><br>
+             &nbsp;&nbsp;Directora carrera Licenciatura en Sistemas de Información </li>
+         <li><b>María de los Ángeles Ferraro </b><br>
+             &nbsp;&nbsp;Directora Departamento de Informática </li>
+         <li><b>Raquel Petris</b><br>
+             &nbsp;&nbsp;Coordinadora Convenio Sadosky-UNNE</li>
+         <li><b>Yanina Medina</b><br>
+             &nbsp;&nbsp;Coordinadora Diplomatura en Programación y Robótica Educativa</li>
+         <li><b>Beatriz Castro Chans</b><br>
+             &nbsp;&nbsp;Responsable del Área de Educación Virtual de FaCENA (UNNE)</li>
+         <li><b>Emanuel Irrazabal</b><br>
+             &nbsp;&nbsp;Director proyecto de investigación en áreas de las Ciencias de la Computación</li>
+         <li><b>Sonia Mariño</b><br>
+             &nbsp;Directora proyecto de investigación en áreas de las Ciencias de la Computación</li>
+     </ul>
+            </td>
+        </tr>
+    </tbody>
+</table>
+   
+ </div>
