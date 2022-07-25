dea050c0f332a77cadb86c1390bb524927e1e01e
ivan sambrana <sambranaivan@gmail.com>
2022-07-25 13:10:56 -0300

    debate

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -34,7 +34,7 @@
       <h5 class="mb-0">
         <button class="btn collapsed verde btn-block text-center" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
         <strong class="text-center" style="font-size:1.2em">
-                             Agenda de Conferencias y Mesas debat &nbsp;<i class="fas fa-chevron-down"></i>
+                             Agenda de Conferencias y Mesas debate &nbsp;<i class="fas fa-chevron-down"></i>
             </strong> 
         </button>
       </h5>
